6b6b7d8bbcc2ff0af987246665fbc79ecb7aa14b
Juan Sebastina Lenis Acosta <jua.lenis01@uceva.edu.co>
2023-09-19 18:47:18 -0500

    se hizo impresion del hola mundo

--- src/main/java/com/mycompany/ejemplogit/Ejemplogit.java
+++ src/main/java/com/mycompany/ejemplogit/Ejemplogit.java
@@ -11,6 +11,7 @@ package com.mycompany.ejemplogit;
 public class Ejemplogit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundo!");
+       
     }
 }
